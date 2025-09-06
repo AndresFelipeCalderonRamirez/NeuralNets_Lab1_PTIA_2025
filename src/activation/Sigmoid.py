b94a84ba3d1f18cd65558b7fbbbc518a3465b98b
Santiago Botero <157855016+LePeanutButter@users.noreply.github.com>
2025-09-06 15:25:52 -0500

    feat(activation): implement base Activation class with inherited functions
    - Defined the abstract `Activation` class with `value` and `derivative` methods.
    - Implemented `use` method to dynamically select activation functions by name.
    - Created `Sigmoid` and `Relu` classes as concrete implementations of the `Activation` class.
    - Added support for future extensions with the activation mapping mechanism.

--- src/activation/Sigmoid.py
+++ src/activation/Sigmoid.py
@@ -1,16 +1,40 @@
 import numpy as np
-import Activation
-from abc import ABC, abstractmethod
+from Activation import Activation
 
-#Documentar los métodos implementados
 class Sigmoid(Activation):
-  """ Función de activación sigmoide. Implementa Activación
-  """
-  def Sigmoid(self):
-    pass
+    """
+    Sigmoid activation function.
+    Implements the activation and its derivative for neural networks.
 
-  def value(self, X: np.ndarray) -> np.ndarray:
-    pass
+    Methods:
+       - value(x: np.ndarray) -> np.ndarray: Computes the Sigmoid output for input x.
+       - derivative(x: np.ndarray) -> np.ndarray: Computes the Sigmoid derivative for input x.
+    """
+    def __init__(self):
+      super().__init__()
 
-  def derivative(self, X: np.ndarray) -> np.ndarray:
-    pass
+    def value(self, x: np.ndarray) -> np.ndarray:
+        """
+        Calculates the output of the Sigmoid activation function.
+        The Sigmoid function is defined as 1 / (1 + exp(-X)).
+
+        Args:
+        - x (np.ndarray): Array of input values.
+
+        Returns:
+        - np.ndarray: Result of applying the Sigmoid function to each element of X.
+        """
+        return 1 / (1 + np.exp(-x))
+
+    def derivative(self, x: np.ndarray) -> np.ndarray:
+        """
+        Calculates the derivative of the Sigmoid activation function.
+        The derivative is computed as Sigmoid(x) * (1 - Sigmoid(x)).
+
+        Args:
+        - x (np.ndarray): Array of input values.
+
+        Returns:
+        - np.ndarray: Derivative of the Sigmoid function for each input value.
+        """
+        return np.exp(-x) / (1 + np.exp(-x)) ** 2
